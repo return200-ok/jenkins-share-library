8a3cc0c97e56dcc228b4022152294b4b66377f62
caolv <caolv@biplus.com.vn>
2022-06-01 15:38:17 +0700

    init

--- resources/com/scripts/linux/deployApplication.sh
+++ resources/com/scripts/linux/deployApplication.sh
@@ -2,21 +2,21 @@ parseFile(){
     grep -w $1 jobScript.map | awk '{print $2}' | head -n1
 }
 
-verify_image_match () {
-    image_require=$(awk -v service="$1" '$1==service {print $3}' jobScript.map)
-    if [[ 1 == 1 ]]; then
-        return 0
-    else 
-        return 1
-    fi
-}
+# verify_image_match () {
+#     image_require=$(awk -v service="$1" '$1==service {print $3}' jobScript.map)
+#     if [[ $2 == *"$image_require"* ]]; then
+#         return 0
+#     else 
+#         return 1
+#     fi
+# }
 
 deployApplication(){
     jobname=$1
     image_version=$2
     deployEnviroment=$3
     deployment_file=$(parseFile $jobname)
-    if $(verify_image_match $jobname $image_version); then
+    # if $(verify_image_match $jobname $image_version); then
         echo "Unlock the deployment"
         sed -ie "s/THIS_STRING_IS_REPLACED_DURING_BUILD/$(date)/g" ${deployment_file} # verify your job with current date
         echo "Reconfig image version"
@@ -34,9 +34,9 @@ deployApplication(){
                 echo "Please check your deploy enviroment"
             ;;
         esac
-    else
-        echo "Fail to deploy, please check your image!"
-    fi
+    # else
+    #     echo "Fail to deploy, please check your image!"
+    # fi
 }
 
 deployApplication $1 $2 $3
