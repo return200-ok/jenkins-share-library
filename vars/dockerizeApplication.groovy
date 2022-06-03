6a807eb4b3d250549a55ec688c218f0eabcee067
caolv <caolv@biplus.com.vn>
2022-06-03 10:21:39 +0700

    init

--- vars/dockerizeApplication.groovy
+++ vars/dockerizeApplication.groovy
@@ -1,3 +1,4 @@
+// Using jenkins plugin
 def call(Map config = [:]) {
     appImage = docker.build "${config.serviceName}"
     docker.withRegistry("${config.DOCKER_REGISTRY_URL}", "${config.REGISTRY_CREDENTIAL}") {
@@ -5,3 +6,9 @@ def call(Map config = [:]) {
         appImage.push("${env.imageTag}")
     }
 }
+// ------------------------------------------------
+// Using bash script
+// def call(Map config = [:]) {
+//     loadLinuxScript(name: 'dockerizeApp.sh')
+//     sh "./dockerizeApp.sh ${config.IMAGE_NAME} ${config.IMAGE_TAG} ${config.DOCKER_REGISTRY_URL} ${config.DOCKER_REGISTRY_PASSWORD}"
+// }
