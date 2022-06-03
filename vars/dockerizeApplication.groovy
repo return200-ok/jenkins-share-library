5fd94e77be8fe2be70f9c1f58164a6ad65bf09ce
caolv <caolv@biplus.com.vn>
2022-06-03 09:33:16 +0700

    init

--- vars/dockerizeApplication.groovy
+++ vars/dockerizeApplication.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
-    appImage = docker.build "${config.imageName}"
+    appImage = docker.build "${config.serviceName}"
     docker.withRegistry("${config.DOCKER_REGISTRY_URL}", "${config.REGISTRY_CREDENTIAL}") {
         appImage.push("latest")
         appImage.push("${env.imageTag}")
