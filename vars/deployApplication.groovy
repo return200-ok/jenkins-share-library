dd790faa783118fa0a1b4974483cf40404ce67f9
caolv <caolv@biplus.com.vn>
2022-06-22 11:32:25 +0700

    cicd

--- vars/deployApplication.groovy
+++ vars/deployApplication.groovy
@@ -1,5 +1,9 @@
 def call(Map config = [:]) {
     loadMapFile(name: 'jobScript.map')
     loadLinuxScript(name: 'deployApplication.sh')
+    sh "mkdir -p k8s-manifest"
+    dir("k8s-manifest") {
+        git credentialsId: 'd281bfc1-1637-4c18-a8a2-cf3567ce567f', url: "${config.k8sManifest}"
+    }
     sh "./deployApplication.sh ${config.jobName} ${config.imageVersion} ${config.deployEnviroment}"
 }
