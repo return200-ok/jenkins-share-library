c69a83ef224bb2f2a8339c921ec783e68ceeba18
caolv <caolv@biplus.com.vn>
2022-06-24 15:37:29 +0700

    cicd

--- resources/com/scripts/linux/bashScript.sh
+++ resources/com/scripts/linux/bashScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # echo Hello $1. Today is $2.
 foo(){
-if [[ "192.168.3.45:8395/java-web-app-master:247" == *"java-web-app"* ]];then
-return 0
-else
-return 1
-fi
+    if [[ "192.168.3.45:8395/java-web-app-master:247" == *"java-web-app"* ]];then
+    return 0
+    else
+    return 1
+    fi
 }
 foo
 #!/bin/bash
